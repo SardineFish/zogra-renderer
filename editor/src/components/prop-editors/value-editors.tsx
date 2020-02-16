f2bafd379dd5266a9e44bbb42da5e5d29bbcbe1a
SardineFish <Sardine@live.cn>
2020-02-16 15:52:38 +0800

    Fix bugs of value editor.

--- editor/src/components/prop-editors/value-editors.tsx
+++ editor/src/components/prop-editors/value-editors.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, FunctionComponent } from "react";
+import React, { ChangeEvent, useState, FunctionComponent, useEffect } from "react";
 import { InputNumber, Input, Dropdown, Menu, Icon, Slider, Checkbox, Switch } from "antd";
 import { SelectParam } from "antd/lib/menu";
 import { clamp } from "../../util/math-util";
@@ -43,6 +43,10 @@ export function NumberEditor(props: ValueEditorProps)
             setValue(value as number);
         }
     };
+    useEffect(() =>
+    {
+        setValue(props.target[props.name] as number);
+    }, [props.target, props.name]);
     return (
         <LabeledEditor className="number-editor" label={props.label || props.name}>
             <InputNumber size="small" value={value} onChange={valueChange} />
@@ -58,6 +62,10 @@ export function StringEditor(props: ValueEditorProps)
         props.target[props.name] = e.target.value || props.target[props.name];
         setValue(props.target[props.name]);
     }
+    useEffect(() =>
+    {
+        setValue(props.target[props.name] as string);
+    }, [props.target, props.name]);
     return (
         <LabeledEditor className="string-editor" label={props.label || props.name}>
             <Input size="small" value={value} onChange={valueChange}/>
@@ -68,23 +76,28 @@ export function StringEditor(props: ValueEditorProps)
 
 export function VectorEditor(props: ValueEditorProps)
 {
-    const [vec, setVec] = useState(props.target[props.name] as number[]);
+    const [vec, setVec] = useState(props.target[props.name] as number[],);
     const valueChange = (i: number) => (value?: number) =>
     {
         if (!isNaN(value as number))
         {
+            const vec = props.target[props.name] as number[];
             vec[i] = value as number;
-            setVec(vec);
+            setVec([...vec]);
             props.target[props.name] = vec;
         }
     };
+    useEffect(() =>
+    {
+        setVec(props.target[props.name] as number[]);
+    }, [props.target, props.name]);
     const axis = ["x", "y", "z", "w"];
     return (
         <LabeledEditor className="vector-editor wrap" label={props.label || props.name}>
             <div className="horizon-layout">
                 {
                     vec.map((num, i) => (<LabeledEditor className="number-editor vector-component-editor inline" label={axis[i]} key={i}>
-                        <InputNumber size="small" defaultValue={num} onChange={valueChange(i)} />
+                        <InputNumber size="small" value={num} onChange={valueChange(i)} />
                     </LabeledEditor>))
                 }
             </div>
@@ -105,6 +118,10 @@ export function QuaternionEditor(props: ValueEditorProps)
             
         }
     };
+    useEffect(() =>
+    {
+        setEular(quat.euler(props.target[props.name] as quat));
+    }, [props.target, props.name]);
     const axis = ["x", "y", "z", "w"];
     return (
         <LabeledEditor className="vector-editor rotation-editor wrap" label={props.label || props.name}>
@@ -146,6 +163,10 @@ export function EnumEditor(props: EnumEditorProps)
         props.target[props.name] = props.options[param.key];
         setSelected(param.key);
     }
+    useEffect(() =>
+    {
+        setSelected(toLabel(props.target[props.name]));
+    }, [props.target, props.name]);
     const menu = (
         <Menu onSelect={select} selectable selectedKeys={[selected]}>
             {
@@ -174,6 +195,10 @@ export function RangeEditor(props: ValueEditorProps & { min: number, max: number
         setValue(clamp(value, props.min, props.max));
         props.target[props.name] = clamp(value, props.min, props.max);
     };
+    useEffect(() =>
+    {
+        setValue(props.target[props.name] as number);
+    }, [props.target, props.name]);
     return (<LabeledEditor className="range-editor" label={props.label || props.name}>
         <div className="horizon-layout">
             <Slider className="range-slider" min={props.min} max={props.max} value={value} onChange={v => valueChange(v as number)} />
@@ -190,6 +215,10 @@ export function BooleanEditor(props: ValueEditorProps)
         setValue(!value);
         props.target[props.name] = value;
     };
+    useEffect(() =>
+    {
+        setValue(props.target[props.name] as boolean);
+    }, [props.target, props.name]);
     return (<LabeledEditor className="bool-editor" label={props.label || props.name}>
         <Switch checked={value} onChange={valueChange} size="small"/>
     </LabeledEditor>)
